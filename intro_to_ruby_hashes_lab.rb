fa94b213d518f5b7adc886f8cc5b7673deb03f0b
Peter Werner <pcw1995@yahoo.com>
2019-09-04 18:11:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def update_counting_hash(hash, key)
     hash[key] +=1 
   else 
     hash[key] = 1 
-    return hash[key]  
+    return hash  
   end
   
   # given a hash an a key as parameters, return an updated hash
